d77c1e4e887834ddf7382e31df4d58bd91c9c9f9
Magnus Jensen <DuckyC@users.noreply.github.com>
2025-05-29 16:39:38 +0200

    Add headless env

--- src/display/main.cpp
+++ src/display/main.cpp
@@ -1,13 +1,19 @@
 #include "main.h"
 
-Controller* controller;
+Controller *controller;
 
 void setup() {
     Serial.begin(115200);
     controller = new Controller();
+#ifdef ENABLE_DISPLAY
     auto ui = new DefaultUI(controller, controller->getPluginManager());
+#endif
     controller->setup();
+#ifdef ENABLE_DISPLAY
     ui->init();
+#else
+    controller->onScreenReady();
+#endif
 }
 
 void loop() {
--- src/display/main.h
+++ src/display/main.h
@@ -2,7 +2,9 @@
 #define MAIN_H
 
 #include "Controller.h"
+#ifdef ENABLE_DISPLAY
 #include <lvgl.h>
 #include <default/DefaultUI.h>
+#endif
 
 #endif // MAIN_H
